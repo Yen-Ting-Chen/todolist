da27bf849cbc735e6cfdd86462f9dae63a15db58
Yen-Ting-Chen <51860102+Yen-Ting-Chen@users.noreply.github.com>
2025-08-14 22:56:32 +0800

    Update settings.py

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'to_do_list.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todolist_db',
+        'USER': 'django',
+        'PASSWORD': 'AmaneKanata',
+        'HOST': '',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
